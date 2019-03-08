fdebc8cb5e2543186c78a98d3640745e2a2d7a85
Péter Tóth <13013768+totpet@users.noreply.github.com>
2019-03-08 15:50:14 +0100

    Add making exact copies of unchanged tables

--- migrator.py
+++ migrator.py
@@ -65,12 +65,98 @@ def main(argv=None):
     old_db = mysql.connector.connect(**old_conf)
     new_db = mysql.connector.connect(**new_conf)
 
-    # TODO
+    migrate(old_db, new_db)
 
     new_db.commit()
 
     new_db.close()
     old_db.close()
+
+
+#: List of tables that are copeid directly, first element is the old table name,
+#: the second is the new (if changed)
+EXACT_COPY = (
+    ('attribute_spec', None),
+    ('attribute_value_organization', None),
+    ('attribute_value_principal', None),
+    ('entitlement', None),
+    ('entitlement_pack', None),
+    ('entitlement_pack_entitlement', None),
+    ('hook', None),
+    ('invitation', None),
+    ('news', None),
+    ('organization', None),
+    ('organization_manager', None),
+    ('organization_principal', None),
+    ('organization_security_domain', None),
+    ('organization_tag', None),
+    ('personal_token', None),
+    ('principal', None),
+    ('role', None),
+    ('role_entitlement', None),
+    ('role_principal', None),
+    ('security_domain', None),
+    ('service', None),
+    ('service_attribute_spec', None),
+    ('service_attribute_value_principal', None),
+    ('service_principal', None),
+    ('service_security_domain', None),
+    ('service_tag', None),
+    ('tag', None),
+
+    # renamed:
+    ('attributevalueorganization_service',
+     'service_attribute_value_organization'),
+)
+
+
+def migrate(old_db, new_db):
+    old_cursor = old_db.cursor()
+    new_cursor = new_db.cursor()
+
+    # disable constraint checks for the current session (referenced rows are
+    # inserted later than their key is referenced)
+    new_cursor.execute('SET FOREIGN_KEY_CHECKS=0')
+
+    # ----------------------
+    # create exact replicas
+
+    for old_table, new_table in EXACT_COPY:
+        new_table = new_table or old_table
+        print(f'{old_table:10s} -> {new_table:10s}')
+
+        select = f'SELECT * FROM {old_table}'
+        print('   ', select)
+        old_cursor.execute(select)
+
+        # column order may vary, so their names are explicitly listed
+        cols = old_cursor.column_names
+        insert = (f"INSERT INTO {new_table} ({','.join(cols)})"
+                  f" VALUES ({','.join(['%s'] * len(cols))})")
+        print('   ', insert)
+
+        BATCH_SIZE = 20000
+        while True:
+            data = old_cursor.fetchmany(size=BATCH_SIZE)
+            if not data:
+                break
+
+            try:
+                new_cursor.executemany(insert, data)
+            except mysql.connector.errors.InterfaceError as ex:
+                print(ex)
+
+                for row in data:
+                    print(row)
+                    new_cursor.execute(insert, row)
+
+            print(f'    {new_cursor.rowcount} rows inserted')
+
+            if len(data) < BATCH_SIZE:
+                break
+
+    new_cursor.close()
+    old_cursor.close()
 
 
 if __name__ == '__main__':
