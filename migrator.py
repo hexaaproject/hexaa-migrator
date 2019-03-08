5480cf7f91b8eac52b99754e386125f29da217e9
Péter Tóth <13013768+totpet@users.noreply.github.com>
2019-03-08 17:57:37 +0100

    Add link creation

--- migrator.py
+++ migrator.py
@@ -73,7 +73,7 @@ def main(argv=None):
     old_db.close()
 
 
-#: List of tables that are copeid directly, first element is the old table name,
+#: List of tables that are copied directly, first element is the old table name,
 #: the second is the new (if changed)
 EXACT_COPY = (
     ('attribute_spec', None),
@@ -111,6 +111,27 @@ EXACT_COPY = (
 
 
 def migrate(old_db, new_db):
+    '''
+    CHANGED:
+        linker_token:
+            In the old version, the token is used to connect to an
+            entitlement_pack, in the new version it enables a link.
+
+    OLD:
+        consent
+        consent_attribute_spec
+
+        organization_entitlement_pack
+    NEW:
+        link
+        link_entitlement
+        link_entitlement_pack
+
+    TODO:
+        Ignore news about consents?
+        select * from news where principal_id=975 and title like '%consent%';
+    '''
+
     old_cursor = old_db.cursor()
     new_cursor = new_db.cursor()
 
@@ -154,6 +175,63 @@ def migrate(old_db, new_db):
 
             if len(data) < BATCH_SIZE:
                 break
+
+    # ----------------------
+    # transform linked orgs and entitlement packs
+
+    # notes:
+    # - `accept_at` is discarded (there is no corresponding column for it in the
+    #   new tables)
+    print('organization_entitlement_pack  -->  link, link_entitlement_pack')
+
+    # this will cause an error if there are linked entitlements between one
+    # service and org with different statuses (in the new version, only one link
+    # is allowed)
+    select = ('SELECT'
+              '   MIN(org_pack.id), organization_id, status,'
+              '   MAX(org_pack.updated_at), MIN(org_pack.created_at),'
+              '   service_id'
+              ' FROM organization_entitlement_pack AS org_pack'
+              ' INNER JOIN entitlement_pack'
+              '   ON entitlement_pack_id = entitlement_pack.id'
+              ' GROUP BY organization_id, service_id, status')
+
+    insert_link = ("INSERT INTO link"
+                   " (id, organization_id, service_id, status, updatedAt,"
+                   "  createdAt)"
+                   " VALUES (%s, %s, %s, %s, %s, %s)")
+
+    print('   ', select)
+    print('   ', insert_link)
+
+    old_cursor.execute(select)
+    data = old_cursor.fetchall()
+    for row in data:
+        (lid, organization_id, status, updated_at,
+         created_at, service_id) = row
+
+        new_cursor.execute(insert_link, (lid, organization_id, service_id,
+                                         status, updated_at, created_at))
+    print(f'    {len(data)} rows transformed\n')
+
+    select = ('SELECT'
+              '   MIN(org_pack.id), entitlement_pack_id'
+              ' FROM organization_entitlement_pack AS org_pack'
+              ' INNER JOIN entitlement_pack'
+              '   ON entitlement_pack_id = entitlement_pack.id'
+              ' GROUP BY organization_id, service_id, entitlement_pack_id')
+    insert_entitlementp = ("INSERT INTO link_entitlement_pack"
+                           " (link_id, entitlement_pack_id)"
+                           " VALUES (%s, %s)")
+    print('   ', select)
+    print('   ', insert_entitlementp)
+
+    old_cursor.execute(select)
+    data = old_cursor.fetchall()
+    for row in data:
+        (lid, entitlement_pack_id) = row
+        new_cursor.execute(insert_entitlementp, (lid, entitlement_pack_id))
+    print(f'    {len(data)} rows transformed')
 
     new_cursor.close()
     old_cursor.close()
